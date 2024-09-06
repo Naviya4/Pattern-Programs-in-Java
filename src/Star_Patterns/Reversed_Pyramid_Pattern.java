d5d62cb608caf07fa460e1cc0f928af8c9ac6e53
Naviya4 <4navindu@gmail.com>
2024-09-06 09:09:52 +0530

    Reversed Pyramid Pattern

--- src/Star_Patterns/Reversed_Pyramid_Pattern.java
+++ src/Star_Patterns/Reversed_Pyramid_Pattern.java
@@ -4,11 +4,18 @@ public class Reversed_Pyramid_Pattern {
 
     public static void Pattern(int n) {
 
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= n; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-
-
+        Pattern(5);
     }
 }
